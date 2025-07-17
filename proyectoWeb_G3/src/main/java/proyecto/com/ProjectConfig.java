077c5ad6a50b0840a573dd717709ead92b89ac62
GabrielOrt2005 <gaortsal4@gmail.com>
2025-07-17 14:53:27 -0600

    intento seguridad :(
    .l.

--- proyectoWeb_G3/src/main/java/proyecto/com/ProjectConfig.java
+++ proyectoWeb_G3/src/main/java/proyecto/com/ProjectConfig.java
@@ -14,16 +14,27 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
     /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
-    
+
     /* localeResolver se utiliza para crear una sesión de cambio de idioma*/
     @Bean
     public LocaleResolver localeResolver() {
@@ -54,5 +65,55 @@ public class ProjectConfig implements WebMvcConfigurer {
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+
+    // DESCOMENTADO: Para que Spring sepa dónde están tus vistas, incluida la de login.
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
+    
+    @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((request) -> request
+            .requestMatchers("/", "/index", "/errores/", "/carrito/", "/pruebas/",
+                             "/reportes/", "/registro/", "/js/", "/producto/",
+                             "/nosotros/", "/webjars/**")
+            .permitAll()
+            .requestMatchers("/producto/nuevo", "/producto/guardar",
+                             "/producto/modificar", "/producto/eliminar",
+                             "/producto/listado", "/nosotros/listado",
+                             "/producto/ver")
+            .hasRole("ADMIN")
+            .requestMatchers("/producto/nuevo",
+                             "/producto/listado",
+                             "/producto/modificar",
+                             "/nosotros/listado",
+                             "/producto/ver")
+            .hasRole("VENDEDOR")
+            .requestMatchers("/nosotros/listado", "/producto/ver")
+            .hasRole("USER")
+        )
+        .formLogin((form) -> form
+            .loginPage("/login").permitAll())
+        .logout((logout) -> logout.permitAll());
+
+    return http.build();
+}
+
+  
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    // DESCOMENTADO: Configura el AuthenticationManager con tu UserDetailsService y el PasswordEncoder.
+    @Autowired
+    public void configurerGlobal (AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService (userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
