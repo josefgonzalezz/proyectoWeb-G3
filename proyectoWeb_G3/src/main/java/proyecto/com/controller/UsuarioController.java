b1139f2a080572d1705f3086b04ca21f6508e863
IsaacAndreysd <isanchez50692@ufide.ac.cr>
2025-08-14 23:17:43 -0700

    commit
    a medias

--- proyectoWeb_G3/src/main/java/proyecto/com/controller/UsuarioController.java
+++ proyectoWeb_G3/src/main/java/proyecto/com/controller/UsuarioController.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package proyecto.com.controller;
 
 import proyecto.com.domain.Usuario;
@@ -20,9 +15,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-
+    
     @Autowired
     private UsuarioService usuarioService;
+    
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
 
     @GetMapping("/listado")
     public String listado(Model model) {
@@ -33,25 +31,23 @@ public class UsuarioController {
     }
 
     @GetMapping("/nuevo")
-    public String usuarioNuevo(Usuario usuario) {
-        return "/usuario/modifica";
+    public String usuarioNuevo(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "/usuario/agregar";
     }
-
-    @Autowired
-    private FirebaseStorageService firebaseStorageService;
 
     @PostMapping("/guardar")
     public String usuarioGuardar(Usuario usuario,
             @RequestParam("imagenFile") MultipartFile imagenFile) {
         if (!imagenFile.isEmpty()) {
-            usuarioService.save(usuario,false);
+            usuarioService.save(usuario, false);
             usuario.setRutaImagen(
                     firebaseStorageService.cargarImagen(
                             imagenFile,
                             "usuario",
                             usuario.getIdUsuario()));
         }
-        usuarioService.save(usuario,true);
+        usuarioService.save(usuario, true);
         return "redirect:/usuario/listado";
     }
 
